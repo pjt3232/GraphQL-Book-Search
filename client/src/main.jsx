df1085bbcb9a3894ec03da250f72c4681e7afa84
Tranel <tranel@Tranels-MacBook-Air.local>
2023-08-01 20:24:37 -0500

    Updated App &main jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,28 +1,36 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import App from './App.jsx'
-import SearchBooks from './pages/SearchBooks'
-import SavedBooks from './pages/SavedBooks'
+import App from './App.jsx';
+import Home from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import Signup from './components/SignupForm';
+import Login from './components/LoginForm';
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <h1 className='display-2'>Wrong page!</h1>,
     children: [
       {
         index: true,
-        element: <SearchBooks />
+        element: <Home />
+      }, {
+        path: '/login',
+        element: <Login />
+      }, {
+        path: '/signup',
+        element: <Signup />
       }, {
         path: '/saved',
         element: <SavedBooks />
       }
     ]
-  }
-])
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
-)
+);
