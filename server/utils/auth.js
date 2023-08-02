0b54dc004e937a5f7fb5ca90412366f188e63cc4
Tranel <tranel@Tranels-MacBook-Air.local>
2023-08-01 20:42:45 -0500

    Updated server side auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,30 +6,29 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+  authMiddleware: function ({ context }, next) {
+    const token = context.headers.authorization;
 
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      throw new Error('You have no token.');
     }
+
+    const tokenValue = token.split(' ').pop().trim();
 
     // verify token and get user data out of it
     try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
+      const { data } = jwt.verify(tokenValue, secret, { maxAge: expiration });
+      context.user = data;
     } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      throw new Error('Invalid token');
     }
 
     // send to next endpoint
-    next();
+    return next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
