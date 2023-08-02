2bec7fc01e8bb4c7128383270873da10f4323a12
Tranel <tranel@Tranels-MacBook-Air.local>
2023-08-01 20:39:44 -0500

    Added server side schemas

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,80 @@
+const User = require('../models/User');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const foundUser = await User.findById(context.user._id);
+                return foundUser;
+            }
+            throw new Error('Not authenticated. Please log in.');
+        },
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ 
+                $or: [{username: email }, { email }]
+            });
+            
+            if (!user) {
+                throw new Error("Can't find this user");
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new Error('Wrong password!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            if (!user) {
+                throw new Error('Something is wrong!');
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (parent, { input }, context) => {
+            if (!context.user) {
+                throw new Error('Not authenticated. Please log in.');
+            }
+
+            try {
+                const updatedUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: input }},
+                    { new: true, runValidators: true }
+                );
+
+                return updatedUser;
+            } catch (err) {
+                console.log(err);
+                throw new Error('Could not save the book');
+            } 
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (!context.user) {
+                throw new Error('Not authenticated. Please log in.');
+            }
+
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+            );
+
+            if (!updatedUser) {
+                throw new Error("Couldn't find user with this id!");
+            }
+
+            return updatedUser;
+        },
+    },
+};
+
+module.exports = resolvers;
